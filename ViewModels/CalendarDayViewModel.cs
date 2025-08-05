b9af085274a42d040047d5f573734bbe4d06ad83
Nikita Vasilev <nikita.vasilyev005@gmail.com>
2025-08-05 19:30:13 +0300

    Add Firebase integration for match data management and update UI bindings

--- ViewModels/CalendarDayViewModel.cs
+++ ViewModels/CalendarDayViewModel.cs
@@ -1,27 +1,70 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Media.Imaging;
 
 namespace FanShop.ViewModels
-
 {
     public class CalendarDayViewModel : INotifyPropertyChanged
     {
         public DateTime Date { get; set; }
         public ObservableCollection<string> Employees { get; set; } = new();
 
-        public bool HasMatch => Match != null;
-        public MatchInfo Match { get; set; }
+        private MatchInfo _match;
+        public MatchInfo Match
+        {
+            get => _match;
+            set
+            {
+                if (_match != value)
+                {
+                    _match = value;
+                    HasMatch = _match != null;
+                    OnPropertyChanged(nameof(Match));
+                }
+            }
+        }
 
-        public bool IsCurrentMonth { get; set; }
+        private bool _hasMatch;
+        public bool HasMatch
+        {
+            get => _hasMatch;
+            set
+            {
+                if (_hasMatch != value)
+                {
+                    _hasMatch = value;
+                    OnPropertyChanged(nameof(HasMatch));
+                }
+            }
+        }
+
+        private bool _isCurrentMonth;
+        public bool IsCurrentMonth
+        {
+            get => _isCurrentMonth;
+            set
+            {
+                if (_isCurrentMonth != value)
+                {
+                    _isCurrentMonth = value;
+                    OnPropertyChanged(nameof(IsCurrentMonth));
+                }
+            }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public class MatchInfo
     {
         public string TeamName { get; set; }
-        public string MatchTime { get; set; }
-        public BitmapImage Time { get; set; }
+        public string Time { get; set; }
+        public BitmapImage Logo { get; set; }
     }
 }
