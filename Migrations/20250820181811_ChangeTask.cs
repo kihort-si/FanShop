f1870ef1c8e26a5bf6d32202de23a16f4e3d0b56
Nikita Vasilev <nikita.vasilyev005@gmail.com>
2026-06-30 01:18:23 +0300

    Add migrations for Task and TaskCategory updates; include DefaultTask column and modify DayTasks structure

---
+++ Migrations/20250820181811_ChangeTask.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FanShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "DayTasks");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "DayTasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskCategoryID",
+                table: "DayTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "DayTasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EndHour",
+                table: "DayTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EndMinute",
+                table: "DayTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StartHour",
+                table: "DayTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StartMinute",
+                table: "DayTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndHour",
+                table: "DayTasks");
+
+            migrationBuilder.DropColumn(
+                name: "EndMinute",
+                table: "DayTasks");
+
+            migrationBuilder.DropColumn(
+                name: "StartHour",
+                table: "DayTasks");
+
+            migrationBuilder.DropColumn(
+                name: "StartMinute",
+                table: "DayTasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskCategoryID",
+                table: "DayTasks",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "DayTasks",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                table: "DayTasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartTime",
+                table: "DayTasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
