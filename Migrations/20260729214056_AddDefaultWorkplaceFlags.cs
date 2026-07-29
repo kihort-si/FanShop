6468acc5e9b7c83f8daaadbb6f90140d03d355e2
Nikita Vasilev <nikita.vasilyev005@gmail.com>
2026-07-30 00:49:18 +0300

    Add default workplace flags to Shops and Positions tables

---
+++ Migrations/20260729214056_AddDefaultWorkplaceFlags.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FanShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultWorkplaceFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDefault",
+                table: "Shops",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDefault",
+                table: "Positions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDefault",
+                table: "Shops");
+
+            migrationBuilder.DropColumn(
+                name: "IsDefault",
+                table: "Positions");
+        }
+    }
+}
